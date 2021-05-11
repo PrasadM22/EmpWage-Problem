19def38dc10427363e13e712780b7804e43658b6
RagahavM22 <pmurhekar8@gmail.com>
2021-05-11 16:57:39 +0530

    Add Changes

--- EmployeeWage/src/Problem/EmployeeWage.java
+++ EmployeeWage/src/Problem/EmployeeWage.java
@@ -9,15 +9,9 @@ public static void main(String[] args)
 		final int IS_FULL_TIME = 1;
 		int EMP_RATE_PER_HOUR =20;
 		final int IS_PART_TIME=2;
-		final int NUM_OF_WORKING_DAYS = 20;
-		int Max_Hrs_In_Month=100;
 		//Computation
-		int totalEmpHrs=0;
-		int totalWorkingDays=0;
-		int totalEmpWage=0;
-		while(totalEmpHrs<=Max_Hrs_In_Month && totalWorkingDays<NUM_OF_WORKING_DAYS){
 		int empHrs=0;
-		totalWorkingDays++;
+		int empWage=0;
 		int empCheck =(int) Math.floor(Math.random()*10)%3;
 		if(empCheck == IS_FULL_TIME || empCheck == IS_PART_TIME)
 			System.out.println("Employee is Present");
@@ -33,11 +27,7 @@ public static void main(String[] args)
 		default:
 			empHrs=0;
 		}
-		totalEmpHrs+=empHrs;
-		int empWage = empHrs * EMP_RATE_PER_HOUR;
-		totalEmpWage+=empWage;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage:"+empWage);
-}
-		System.out.println("Total Emp Wage:" + totalEmpWage);
 }
 }
