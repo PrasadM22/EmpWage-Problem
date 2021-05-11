6cf407b238779d1c06b6cf01afc23db893814e8e
RagahavM22 <pmurhekar8@gmail.com>
2021-05-11 17:26:32 +0530

    Add Changes

--- EmployeeWage/src/Problem/EmployeeWage.java
+++ EmployeeWage/src/Problem/EmployeeWage.java
@@ -1,43 +1,43 @@
 package Problem;
 
-public class EmployeeWage
-{
-public static void main(String[] args)
-{
-		System.out.println("Welcome to Employee Wage Computation");
-		//Constant
-		final int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR =20;
-		final int IS_PART_TIME=2;
-		final int NUM_OF_WORKING_DAYS = 20;
-		int Max_Hrs_In_Month=100;
-		//Computation
-		int totalEmpHrs=0;
-		int totalWorkingDays=0;
-		int totalEmpWage=0;
-		while(totalEmpHrs<=Max_Hrs_In_Month && totalWorkingDays<NUM_OF_WORKING_DAYS){
-		int empHrs=0;
-		totalWorkingDays++;
-		int empCheck =(int) Math.floor(Math.random()*10)%3;
-		if(empCheck == IS_FULL_TIME || empCheck == IS_PART_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
-		switch (empCheck) {
-		case (IS_FULL_TIME):
-		     empHrs=8;
-		     break;
-		case IS_PART_TIME:
-		     empHrs=4;
-		     break;
+public class EmployeeWage {
+	int totalEmpHrs, totalEmpWage, totalWorkingDays;
+	public static final int Is_Part_Time =1;
+	public static final int Is_Full_Time =2;
+	public static final int Emp_Rate_Per_Hour = 20;
+	public static final int Num_of_Working_Days= 20;
+	public static final int Max_Hrs_In_Month = 100;
+	
+	EmployeeWage(){
+		int totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
+	}
+	
+	public  int Calculate_Wage()
+	{
+		while( totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_of_Working_Days)
+		{	int empHrs =0 ;
+			totalWorkingDays++;
+	 	int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+		switch(empCheck) {
+		case Is_Full_Time:
+			empHrs = 8;
+			break;
+		case Is_Part_Time:
+			empHrs=4;
+			break;
 		default:
-			empHrs=0;
+			empHrs= 0;
 		}
-		totalEmpHrs+=empHrs;
-		int empWage = empHrs * EMP_RATE_PER_HOUR;
-		totalEmpWage+=empWage;
-		System.out.println("Emp Wage:"+empWage);
-}
-		System.out.println("Total Emp Wage:" + totalEmpWage);
+		totalEmpHrs += empHrs;
+		int empWage = empHrs * Emp_Rate_Per_Hour;
+		totalEmpWage += empWage;
+		System.out.println("Emp Wage :" + empWage );
+	}
+		return totalEmpWage;
+	}
+	public static void main(String[] args)
+	{	
+	EmployeeWage obj = new EmployeeWage();
+	System.out.println("Total Emp Wage:"+obj.Calculate_Wage());
 }
 }
